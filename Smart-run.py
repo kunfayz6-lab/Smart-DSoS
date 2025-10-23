c890ca6d44ab76001724e613d554239e16c6fbac
kunfayz6-lab <kunfayz6@gmail.com>
2025-10-23 12:57:43 +0700

    Update Smart-run.py

--- Smart-run.py
+++ Smart-run.py
@@ -12,70 +12,70 @@ if not __name__ == "__main__":
       
 class ConsoleColors:
     def warna():
-\033[2mgray = abu-abu
-\033[31mred = merah
-\033[32mgreen = hijau
-\033[33myellow = kuning
-\033[34mblue = biru
-\033[35mpurple = ungu
-\033[36mcyan = cyan
-\033[90mdark gray = abu-abu gelap
+'\033[2mgray = abu-abu
+'\033[31mred = merah
+'\033[32mgreen = hijau
+'\033[33myellow = kuning
+'\033[34mblue = biru
+'\033[35mpurple = ungu
+'\033[36mcyan = cyan
+'\033[90mdark gray = abu-abu gelap
 
 def warnabg():
 Background color
-\033[7mwhite = putih
-\033[40mblack = hitam
-\033[41mred = merah
-\033[42mgreen = hijau
-\033[43myellow = kuning
-\033[44mblue = biru
-\033[45mpurple = ungu
-\033[46mcyan = cyan
+'\033[7mwhite = putih
+'\033[40mblack = hitam
+'\033[41mred = merah
+'\033[42mgreen = hijau
+'\033[43myellow = kuning
+'\033[44mblue = biru
+'\033[45mpurple = ungu
+'\033[46mcyan = cyan
 warnabg()
 
 def warnacerah():
-\033[91mbright red = merah cerah
-\033[92mbright green = hijau cerah
-\033[93mbright yellow = kuning cerah
-\033[94mbright blue = biru cerah
-\033[95mbrightpurple=Ungu cerah
-\033[96mbright cyan=cyan cerah
-\033[97mbright white=putih cerah
+'\033[91mbright red = merah cerah
+'\033[92mbright green = hijau cerah
+'\033[93mbright yellow = kuning cerah
+'\033[94mbright blue = biru cerah
+'\033[95mbrightpurple=Ungu cerah
+'\033[96mbright cyan=cyan cerah
+'\033[97mbright white=putih cerah
 
 warnacerah()
-\033[47mbright gray=abu² cerah
-\033[100mdarklightgray=abu-abu gelap
-\033[101mbright red=merah cerah
-\033[102mbright green=hijau cerah
-\033[104mbright blue=biru cerah
-\033[105mbright purple=ungu cerah
-\033[106mbright cyan=cyan cerah
-\033[103mbright yellow=kuning cerah
+'\033[47mbright gray=abu² cerah
+'\033[100mdarklightgray=abu-abu gelap
+'\033[101mbright red=merah cerah
+'\033[102mbright green=hijau cerah
+'\033[104mbright blue=biru cerah
+'\033[105mbright purple=ungu cerah
+'\033[106mbright cyan=cyan cerah
+'\033[103mbright yellow=kuning cerah
 
 peint("""
-\033[96m\033[102m
-\033[96m
-\033[96m
-\033[96m
-\033[96m
-\033[96m
-\033[96m
-\033[96m
-\033[96m
-\033[96m
-\033[96m
-\033[96m
+\033[96m║\033[102m
+\033[96m║\033[102m║████████╚╗
+\033[96m║\033[102m║██╔═══╗██║
+\033[96m║\033[102m║██║     ║██║
+\033[96m║\033[102m║██║     ║██║
+\033[96m║\033[102m║██║     ║██║
+\033[96m║\033[102m║██║     ║██║
+\033[96m║\033[102m║████████╔╝
+\033[96m║\033[102m║██╔══╗██╚╗
+\033[96m║\033[102m║██║    ╚╗██║
+\033[96m║\033[102m╚══╝      ╚══╝
+\033[96m║\033[102m
 """)
 print(ConsoleColors.BOLD + ConsoleColors.WARNING + '''
  ____       ____      _____           
 
-         written by:••••
+        written by:••••
                
       ''')
     
 def getport():
     try:
-        p = int(input(ConsoleColors.BOLD + ConsoleColors.OKGREEN + "Port:\r\n"))
+        p = int(input(ConsoleColors.BOLD +ConsoleColors.OKGREEN + "Port:\r\n"))
         return p
     except ValueError:
         print(ConsoleColors.BOLD + ConsoleColors.WARNING + "ERROR Port must be a number, Set Port to default " + ConsoleColors.OKGREEN + "80")
